658eb5defad8b6dc2a410d8e92d13cb1e01b703d
Andrey Ovsyannikov <andmit8@mail.ru>
2023-01-30 20:40:53 +0300

    get data edit

--- src/data/preprocess_data.py
+++ src/data/preprocess_data.py
@@ -1,3 +1,4 @@
+import os
 import string
 
 import click
@@ -42,24 +43,27 @@ class DataPreprocessor:
 
 @click.command()
 @click.argument("input_filepath_df", type=click.Path(exists=True))
-@click.argument("label_name")
-@click.argument("output_filepath_train_df", type=click.Path())
-@click.argument("output_filepath_test_df", type=click.Path())
-def preprocess_data(input_filepath_df, label_name,
-                    output_filepath_train_df, output_filepath_test_df):
+@click.argument("output_folderpath", type=click.Path())
+@click.argument("label_names", nargs=-1)
+def preprocess_data(input_filepath_df, output_folderpath,
+                    label_names):
     preprocessor = DataPreprocessor()
     df = pd.read_csv(input_filepath_df)
-    preprocessed_df = pd.DataFrame()
-    preprocessed_df['texts'] = preprocessor.preprocess_texts(df['texts'])
-    preprocessed_df['labels'] = df[label_name]
-    train_df, test_df = train_test_split(preprocessed_df, test_size=0.2,
-                                         random_state=42, shuffle=True,
-                                         stratify=preprocessed_df['labels'])
-    train_df.to_csv(output_filepath_train_df, index=False)
-    test_df.to_csv(output_filepath_test_df, index=False)
+    for label_name in label_names:
+        preprocessed_df = pd.DataFrame()
+        preprocessed_df['texts'] = preprocessor.preprocess_texts(df['texts'])
+        preprocessed_df['labels'] = df[label_name]
+        train_df, test_df = train_test_split(preprocessed_df, test_size=0.2,
+                                             random_state=42, shuffle=True,
+                                             stratify=preprocessed_df['labels'])
+        train_file_name = label_name + '_train.csv'
+        test_file_name = label_name + '_test.csv'
+        train_df.to_csv(os.path.join(output_folderpath, train_file_name), index=False)
+        test_df.to_csv(os.path.join(output_folderpath, test_file_name), index=False)
 
 
 if __name__ == "__main__":
     preprocess_data()
 
 # python -m src.data.preprocess_data data/raw/labeled_texts_fixed_binary.csv nationality data/processed/train_nationality.csv data/processed/test_nationality.csv
+# python -m src.data.preprocess_data data/raw/labeled_texts_fixed_binary.csv data/processed nationality families sex limit
